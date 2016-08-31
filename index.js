aaf4024507701bcc98b5ec945840a6466adb2e62
Davide Papagni <davide@algonauti.com>
2016-08-31 11:45:17 +0200

    load userengage widget javascript

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-userengage'
+  name: 'ember-userengage',
+
+  contentFor: function(type, config) {
+    var content = '';
+    if (type === 'head') {
+      var src = 'https://widget.userengage.io/widget.js';
+      content = '<script data-cfasync="false" src="'+src+'"></script>';
+    }
+    return content;
+  }
+
 };
